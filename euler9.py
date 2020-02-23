95810f19e9d74fec4c30b94fdc97eca972fbdda5
leo0842 <kunga077@gmail.com>
2020-02-23 21:15:54 +0900

    9번 문제 풀이

--- euler9.py
+++ euler9.py
@@ -5,7 +5,7 @@ def pytha_products(n):                      ## 함수 생성, 합을 인자로 �
     for a in range(1, n):                   ## a 에 1부터 n 까지 넣을 예정
         for b in range(a, n):               ## b 에 a부터 n 까지 넣을 예정 a 이하는 이미 a에서 넣을 것이기에 필요없음
             c = n - a - b                   ## c 는 나머지
-            if c <= b:                      ## b 가 c 보다 커지면 break. 이 함수 안에서는 c를 변수로 간주하기 때문에
+            if c <= b:                      ## b 가 c 보다 커지면 break. 이 함수 안에서는 c를 빗변으로 간주하기 때문에
                 break
             if c ** 2 == a ** 2 + b ** 2:   ## 피타고라스 정리 확인
                 pytha = True                ## 성립하면 pytha 변수가 True로 바뀜
